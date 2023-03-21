fab35525e1c75564a902c4b32ffa97d713f5595e
Ranhuiryan <qdgjhrh@outlook.com>
2023-03-21 21:43:24 +0800

    fix tts

--- chatGPTEx/static/js/tts.js
+++ chatGPTEx/static/js/tts.js
@@ -39,11 +39,9 @@ async function TTS(text) {
 </speak>`;
     const ChineseSsml = `<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xml:lang="zh-CN">
 <voice name="zh-CN-XiaoxiaoNeural">
-    <s /><mstts:express-as style="affectionate">
     <prosody rate="+10.00%" pitch="+15.00%">
         ${text}
     </prosody>
-    </mstts:express-as><s />
 </voice>
 </speak>`;
     var ssml;
